04296bb616ee395c9604284b056d9ecd17e5186e
shahzaib khan <shahzaibtp@gmail.com>
2020-07-26 13:51:22 +0500

    Todo App Completed

---
+++ todo.js
@@ -0,0 +1,29 @@
+$(".txtb").on("keyup",function(e){
+    if(e.keyCode == 13 && $(".txtb").val() != "")
+    {
+        var task = $("<div class = 'task'></div>").text($(".txtb").val());
+        var del = $("<i class = 'fa fa-trash-alt'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                p.remove();
+
+            })
+        })
+
+        var check = $("<i class = 'fa fa-check'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                $(".comp") .append(p)
+                p.fadeIn();
+
+            })
+            $(this).remove();
+        })
+
+        task.append(del,check);
+        $(".notcomp").append(task);
+        $(".txtb").val("")
+
+    }
+    
+});
